df84a0a2a3cedb3190d394eeede23c7cc0c7d784
deepakbonthu <deepakbonthu1@gmail.com>
2026-04-08 17:56:27 +0530

    Update ingestion_pipeline.py

--- week0/phase3/ingestion_pipeline.py
+++ week0/phase3/ingestion_pipeline.py
@@ -19,33 +19,20 @@ df = spark.createDataFrame(data, ["customer_id", "name", "city", "amount"])
 
 df.show()
 
-# -------------------------
-# Transform (Cleaning)
-# -------------------------
 clean_df = df.dropna()
 
 filtered_df = clean_df.filter(clean_df.amount > 0)
 
 filtered_df.show()
 
-# -------------------------
-# Business Logic
-# -------------------------
-
-# 1. City-wise revenue
 city_revenue = filtered_df.groupBy("city").sum("amount")
 city_revenue.show()
 
-# 2. Repeat customers (>1 order)
 repeat_customers = filtered_df.groupBy("customer_id") \
 .agg(count("*").alias("order_count")) \
 .filter("order_count > 1")
-
 repeat_customers.show()
 
-# -------------------------
-# Load (Final Output)
-# -------------------------
 final_df = filtered_df.groupBy("customer_id") \
 .agg(sum("amount").alias("total_spend"))
 
